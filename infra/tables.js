a3c1508fb09a363dcb131ede4e124d1ac309a9bd
matheusmenezs <matheushm1798@gmail.com>
2022-02-07 08:21:26 -0300

    feat: create table in database

---
+++ infra/tables.js
@@ -0,0 +1,20 @@
+class Tables{
+    init(connection){
+        this.connection = connection
+
+        this.createTable()
+    }
+    createTable(){
+        const sql = 'CREATE TABLE IF NOT EXISTS Attendance(id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(30) NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY(id))'
+
+        this.connection.query(sql, erro => {
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log('Table created successfully')
+            }
+        })
+    }
+}
+
+module.exports = new Tables
