3f23a26a0da843b2f6225dde9ea3bb40b361c7f5
matheusmenezs <matheushm1798@gmail.com>
2022-02-07 08:23:53 -0300

    feat: run database connection

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const customExpress = require('./config/customExpress')
+const connection = require('./infra/connection')
+const table = require('./infra/tables')
 
-const app = customExpress()
+connection.connect(error => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log('connection established')
+        table.init(connection)
 
-app.listen(3000, console.log('server running on port 3000'))
+        const app = customExpress()
+        app.listen(3000, console.log('Server running on port 3000'))
+    }
+})
+
