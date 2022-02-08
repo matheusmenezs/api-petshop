8afb6ab1975bc693f993d59c20107aabf76ffa7d
matheusmenezs <matheushm1798@gmail.com>
2022-02-08 08:03:58 -0300

    feat: add body-parser library resources

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const consign = require('consign')
+const bodyparser = require('body-parser')
 
 module.exports = () => {
     const app = express()
+    app.use(bodyparser.urlencoded({extended:false}))
+    app.use(bodyparser.json())
     consign()
         .include('controller')
         .into(app)
