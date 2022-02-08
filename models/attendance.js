830a4c9db55ec8b19885122b4e9a3de4ce1be099
matheusmenezs <matheushm1798@gmail.com>
2022-02-08 08:07:30 -0300

    feat: create of methods and queries

---
+++ models/attendance.js
@@ -0,0 +1,65 @@
+const connection = require('../infra/connection')
+
+class Attendance{
+    List(res){
+        const sql = 'SELECT * FROM Attendance'
+
+        connection.query(sql, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(resultados)
+            }
+        })
+    }
+
+    Search(id, res){
+        const sql = 'SELECT * FROM Attendance WHERE id = ?'
+
+        connection.query(sql, id, res, (erro, resultados) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(201).json(resultados)
+            }
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Add(data, res){
+        const sql = 'INSERT INTO Attendance SET ?'
+
+        connection.query(sql, data, erro => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(201).json(data)
+            }
+        })
+    }
+
+}
+
+module.exports = new Attendance
