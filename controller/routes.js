727a8e8bb326dd5b7f506a328ce003fa6304de65
matheusmenezs <matheushm1798@gmail.com>
2022-02-08 08:06:04 -0300

    feat: create routes to list and add calls

--- controller/routes.js
+++ controller/routes.js
@@ -1,8 +1,15 @@
+const Attendance = require('../models/attendance')
+
 module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
-        res.send('executing method GET')
+        Attendance.List(res)
+    })
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = req.params.id
+        Attendance.Search(id, res)
     })
     app.post('/atendimentos', (req, res) => {
-        res.send('executing method POST')
+        const data = req.body
+        Attendance.Add(data, res)
     })
 }
