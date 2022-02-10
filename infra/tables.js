ec796e76d8b8c21de798d1475f56cc4e67384038
matheusmenezs <matheushm1798@gmail.com>
2022-02-10 16:59:56 -0300

    feat: change database table

--- infra/tables.js
+++ infra/tables.js
@@ -5,7 +5,7 @@ class Tables{
         this.createTable()
     }
     createTable(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Attendance(id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(30) NOT NULL, status varchar(20) NOT NULL, comments text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Attendance(id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(30) NOT NULL, status varchar(20) NOT NULL, comments text, date datetime NOT NULL, creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id))'
 
         this.connection.query(sql, erro => {
             if(erro){
