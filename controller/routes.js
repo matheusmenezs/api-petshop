faaf5611a83cb9c6740e5b2355d1b65091540003
matheusmenezs <matheushm1798@gmail.com>
2022-02-10 18:50:29 -0300

    feat: add delete request route

--- controller/routes.js
+++ controller/routes.js
@@ -4,17 +4,30 @@ module.exports = (app) => {
     app.get('/atendimentos', (req, res) => {
         Attendance.List(res)
     })
+
     app.get('/atendimentos/:id', (req, res) => {
         const id = req.params.id
         Attendance.Search(id, res)
     })
+
     app.post('/atendimentos', (req, res) => {
         const attendance = req.body
         Attendance.Add(attendance, res)
     })
+
     app.patch('/atendimentos/:id', (req, res) => {
         const id = req.params.id
         const attendance = req.body
-        Attendance.Add(id, attendance, res)
+        Attendance.Update(id, attendance, res)
+    })
+    
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = req.params.id
+        Attendance.deleteId(parseInt(id), res)
+    })
+
+    app.delete('/atendimentos', (req, res) => {
+        const attendance = req.body
+        Attendance.Delete(attendance, res)
     })
 }
