8ae3403ca32511a21210dd8f33cde44290fb3ec0
matheusmenezs <matheushm1798@gmail.com>
2022-02-10 18:52:00 -0300

    feat: change update and delete route

--- models/attendance.js
+++ models/attendance.js
@@ -4,11 +4,11 @@ class Attendance{
     List(res){
         const sql = 'SELECT * FROM Attendance'
 
-        connection.query(sql, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro)
+        connection.query(sql, (error, results) => {
+            if(error){
+                res.status(400).json(error)
             }else{
-                res.status(200).json(resultados)
+                res.status(200).json(results)
             }
         })
     }
@@ -16,11 +16,11 @@ class Attendance{
     Search(id, res){
         const sql = 'SELECT * FROM Attendance WHERE id = ?'
 
-        connection.query(sql, id, res, (erro, resultados) => {
-            if(erro){
-                res.status(400).json(erro)
+        connection.query(sql, id, (error, results) => {
+            if(error){
+                res.status(400).json(error)
             }else{
-                res.status(201).json(resultados)
+                res.status(201).json(results)
             }
         })
     }
@@ -36,16 +36,64 @@ class Attendance{
         }else{
             const sql = 'INSERT INTO Attendance SET ?'
 
-            connection.query(sql, attendance, erro => {
-                if(erro){
-                    res.status(400).json(erro)
+            connection.query(sql, attendance, error => {
+                if(error){
+                    res.status(400).json(error)
                 }else{
                     res.status(201).json(attendance)
                 }
             })
         }
         
+    }
 
+    Update(id, attendance, res){
+
+        attendance.date = moment(attendance.date, 'DD/MM/YYYY').format('YYYY-MM-DD')
+        const message = 'Date is invalid'
+
+        if (moment (attendance.creation_date).isAfter(attendance.date)){
+            res.status(400).json(message)
+        }else{
+            const sql  = 'UPDATE Attendance SET ? WHERE id = ?'
+
+            connection.query(sql, [attendance, id], (error, results) => {
+                if(error){
+                    res.status(400).json(error)
+                }else{
+                    res.status(200).json(attendance)
+                }
+            })
+        }
+    }
+
+    deleteId(id, res){
+        const sql = 'DELETE FROM Attendance WHERE id = ?'
+
+        connection.query(sql, id, (erro, results) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(200).json(results)
+            }
+        })
+
+    }
+
+    Delete(attendance, res){
+        const id = attendance.id
+        
+        const sql = 'DELETE FROM Attendance WHERE id = ?'
+
+        connection.query(sql, id, (erro, results) => {
+            if(erro){
+                res.status(400).json(erro)
+            }else{
+                res.status(400).json(results)
+            }
+        })
+
+        
     }
 
 }
